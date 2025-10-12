c5e210a0915c8088b7b61d138ed50ecf5f1f9733
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-12 23:08:01 +0530

    npx create-react-app & npm create vite@latest

--- Intro/intro.js
+++ Intro/intro.js
@@ -1,1 +1,7 @@
-//
+// React -> make easy to manage and build complex frontend
+
+// “React is a JavaScript library developed by Facebook for building fast and interactive user interfaces.
+// It uses a component-based architecture, where the UI is divided into reusable pieces called components.
+// React uses a virtual DOM to efficiently update and render only the parts of the page that change, improving performance.
+// It also supports features like unidirectional data flow, JSX syntax, and hooks for managing state and side effects.
+// Overall, React makes it easier to build scalable, maintainable, and dynamic web applications.”
