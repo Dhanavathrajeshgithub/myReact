5e608f00fc02594b612b208ce5d32f78dadac222
Dhanavath Rajesh <dhanavathrajeshgithub@gmail.com>
2025-10-27 21:44:03 +0530

    tailwind setup & stored condifured in main.jsx & components header and footer just initialized in app.jsx

--- 14MegaBlock_appWrite/src/App.jsx
+++ 14MegaBlock_appWrite/src/App.jsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components/index.js";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A blog app with app write</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((err) => {
+        dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <p>Loading.....</p>
   );
 }
 
